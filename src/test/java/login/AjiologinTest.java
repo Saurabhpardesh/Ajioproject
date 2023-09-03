c4a89e36546814f31fcdf777c667e4a773467ad7
Dell <Dell@DESKTOP-O0U4QRU>
2023-09-03 14:17:33 +0530

    [Saurabh] its my second push

---
+++ src/test/java/login/AjiologinTest.java
@@ -0,0 +1,17 @@
+package login;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AjiologinTest {
+  @Test
+  public void AjioTest() {
+	  System.setProperty("webdriver.chrome.driver","D:\\Chrome downlode file\\Chrome driver Selenium\\chromedriver-win64\\chromedriver.exe");
+	  Reporter.log("Opening  Ajio",true);
+	  WebDriver driver =new ChromeDriver ();
+	  driver.get("https://www.ajio.com/");
+	  driver.quit();
+  }
+}
